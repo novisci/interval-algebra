2815e8b13746cd9757ef5744fb2bc1c89ea55d15
bsaul <bradleysaul@gmail.com>
2019-11-07 14:40:45 -0500

    Gets test back up and running

--- test/IntervalAlgebraSpec.hs
+++ test/IntervalAlgebraSpec.hs
@@ -1,34 +1,43 @@
+{-# LANGUAGE FlexibleInstances #-}
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import IntervalAlgebra as IA
-import IntervalAlgebra.IntervalCombiner
+import Data.Maybe
+--import IntervalAlgebra.IntervalCombiner
 import Control.Monad
 
 xor :: Bool -> Bool -> Bool
 xor a b = a /= b
 
-instance Arbitrary Period where
-  arbitrary = liftM2 safePeriod arbitrary arbitrary
+instance Arbitrary (Intrvl Int) where
+  arbitrary = liftM2 safeInterval arbitrary arbitrary
 
--- | Safely create a period from two Ints  
-safePeriod :: Int -> Int -> Period
-safePeriod x y 
-  | x < y     = period x (y + 1)
-  | otherwise = period y (x + 1)
+makePos :: Int -> Int
+makePos x
+  | x == 0    = x + 1
+  | x <  0    = negate x
+  | otherwise = x
 
--- | Safely create a singleton Period or empty list from two Ints 
-safePeriod' :: Int -> Int -> [Period]
-safePeriod' x y 
-  | y < x    = []
-  | otherwise = [period x (y + 1)]
+
+-- | Safely create a valid 'Intrvl Int' from two Ints. 
+safeInterval :: Int -> Int -> Intrvl Int
+safeInterval b dur = interval' b (makePos dur)
+
+-- | Create a 'Maybe Intrvl Int' from two Ints.
+safeInterval' :: Int -> Int -> Maybe (Intrvl Int)
+safeInterval' a b 
+    | b <= a    = Nothing
+    | otherwise = Just $ validInterval a b
+
 
 -- | A set used for testing M1 defined so that the M1 condition is true.
 data M1set = M1set { 
-     m11 :: Period
-   , m12 :: Period
-   , m13 :: Period
-   , m14 :: Period }
+     m11 :: Intrvl Int
+   , m12 :: Intrvl Int
+   , m13 :: Intrvl Int
+   , m14 :: Intrvl Int }
    deriving (Show)
 
 instance Arbitrary M1set where
@@ -39,22 +48,27 @@ instance Arbitrary M1set where
     c <- arbitrary
     return $ m1set x a b c
 
-m1set :: Period -> Int -> Int -> Int -> M1set
+-- | Smart constructor of 'M1set'.
+m1set :: Intrvl Int -> Int -> Int -> Int -> M1set
 m1set x a b c = M1set p1 p2 p3 p4
-  where p1 = x
-        p2 = expandr ((abs a) + 1) $ point $ end x 
-        p3 = expandr ((abs b) + 1) $ point $ end x 
-        p4 = expandl ((abs c) + 1) $ point $ begin p2 
-  -- NOTE: adding 1 to the expansion is to ensure that the intervals have
-  -- a nonzero duration (i.e. that the periods are not points).
-  -- Consider the following falsification:
-  --    Falsified (after 1 test):
-  --       M1set {i = (-1, 0), j = (0, 1), k = (0, 0), l = (0, 0)}
-  -- Here k == l and both have duration 0, but this violates ML1. 
-  -- My (B. Saul) current (2019-07-17) understanding is that these axioms
-  -- only apply to *interval* time and not to points; hence the falsification
-  -- shown is not a valid test of the axiom since it includes points.
+  where p1 = x                          -- interval i in prop_IAaxiomM1
+        p2 = safeInterval (end x) a     -- interval j in prop_IAaxiomM1
+        p3 = safeInterval (end x) b     -- interval k in prop_IAaxiomM1
+        p4 = expandl (makePos c) pt     -- interval l in prop_IAaxiomM1
+        pt = validInterval (begin p2 - 1) (begin p2)
 
+{-
+
+ ** Axiom M1
+
+ The first axiom of Allen and Hayes (1987) states that if "two periods both
+ meet a third, thn any period met by one must also be met by the other." 
+ That is:
+
+ \[
+   \forall i,j,k,l s.t. (i:j & i:k & l:j) \implies l:k
+ \] 
+-}
 prop_IAaxiomM1 :: M1set -> Property
 prop_IAaxiomM1 x = 
   (i `meets` j && i `meets` k && l `meets` j) ==> (l `meets` k)
@@ -63,11 +77,12 @@ prop_IAaxiomM1 x =
         k = m13 x
         l = m14 x
 
+-- | A set used for testing M2 defined so that the M2 condition is true.
 data M2set = M2set {
-    m21 :: Period
-  , m22 :: Period
-  , m23 :: Period
-  , m24 :: Period }
+    m21 :: Intrvl Int
+  , m22 :: Intrvl Int
+  , m23 :: Intrvl Int
+  , m24 :: Intrvl Int }
   deriving (Show)
 
 instance Arbitrary M2set where
@@ -78,28 +93,65 @@ instance Arbitrary M2set where
     c <- arbitrary
     return $ m2set x a b c
 
-m2set :: Period -> Period -> Int -> Int -> M2set
+-- | Smart constructor of 'M2set'.
+m2set :: Intrvl Int -> Intrvl Int -> Int -> Int -> M2set
 m2set x y a b = M2set p1 p2 p3 p4
-  where p1 = x
-        p2 = expandr (abs a) $ point $ end x 
-        p3 = y
-        p4 = expandr (abs b) $ point $ end y
+  where p1 = x                          -- interval i in prop_IAaxiomM2
+        p2 = safeInterval (end x) a     -- interval j in prop_IAaxiomM2
+        p3 = y                          -- interval k in prop_IAaxiomM2
+        p4 = safeInterval (end y) b     -- interval l in prop_IAaxiomM2
+
+{-
+
+ ** Axiom M2
+
+ The second interval axiom of Allen and Hayes (1987):
+
+ \[
+   \forall i,j,k,l s.t. (i:j & k:l) \implies 
+     i:l \oplus 
+     (\exists m s.t. i:m:l) \oplus
+     (\exists m s.t. k:m:j) 
+ \] 
+-}
 
 prop_IAaxiomM2 :: M2set -> Property
 prop_IAaxiomM2 x =
   (i `meets` j && k `meets` l) ==> 
     (i `meets` l)  `xor`  
-    (not $ null m) `xor`
-    (not $ null n)
+    (not $ isNothing m) `xor`
+    (not $ isNothing n)
     where i = m21 x
           j = m22 x
           k = m23 x
           l = m24 x
-          m = safePeriod' (end $ i) (begin $ l)
-          n = safePeriod' (end $ k) (begin $ j)
+          m = safeInterval' (end $ i) (begin $ l)
+          n = safeInterval' (end $ k) (begin $ j)
 
-prop_IAaxiomML1 :: Period -> Property
+{-
+
+ ** Axiom ML1
+
+ An interval cannot meet itself
+
+ \[
+   \forall i \lnot i:i
+ \] 
+-}
+
+prop_IAaxiomML1 :: Intrvl Int -> Property
 prop_IAaxiomML1 x = not (x `meets` x) === True
+
+{-
+
+ ** Axiom ML2
+
+ If i meets j then j does not meet i.
+
+ \[
+   \forall i,j i:j \implies \lnot j:i
+ \] 
+-}
 
 prop_IAaxiomML2 :: M2set -> Property
 prop_IAaxiomML2 x =
@@ -107,107 +159,189 @@ prop_IAaxiomML2 x =
   where i = m21 x
         j = m22 x
 
-prop_IAaxiomM3 :: Period -> Property
-prop_IAaxiomM3 x = 
-   (y `meets` x && x `meets` z) === True
-   where y = expandl 1 $ point $ begin x
-         z = expandr 1 $ point $ end x
+
+{-
+
+ ** Axiom M3
+
+ Time does not start or stop:
+
+ \[
+   \forall i \exists j,k s.t. j:i:k
+ \] 
+-}
+
+prop_IAaxiomM3 :: Intrvl Int -> Property
+prop_IAaxiomM3 i = 
+   (j `meets` i && i `meets` k) === True
+   where j = validInterval (begin i - 1) (begin i)
+         k = validInterval (end i) (end i + 1)
+
+{-
+
+** Axiom M4
+
+If two meets are separated by intervals, then this sequence is a longer interval.
+
+ \[
+   \forall i,j i:j \implies (\exists k,m,n s.t m:i:j:n & m:k:n) 
+ \] 
+-}
 
 prop_IAaxiomM4 :: M2set -> Property
 prop_IAaxiomM4 x = 
    ((m `meets` i && i `meets` j && j `meets` n) &&
     (m `meets` k && k `meets` n)) === True
-   where i  = m21 x
-         j  = m22 x
-         m = expandl 1 $ point $ begin $ i
-         n = expandr 1 $ point $ end $ j
-         k = period (end m) (begin n)
+   where i = m21 x
+         j = m22 x
+         m = validInterval (begin i - 1) (begin i)
+         n = validInterval (end j) (end j + 1)
+         k = validInterval (end m) (begin n)
 
-prop_IAbefore :: Period -> Period -> Property
+
+{-
+
+** Axiom M5
+
+If two meets are separated by intervals, then this sequence is a longer interval.
+
+ \[
+   \forall i,j,k,l (i:j:l & i:k:l) \seteq j = l
+ \] 
+-}
+
+-- TODO
+
+{-
+
+** Axiom M4.1
+
+Ordered unions
+
+ \[
+   \forall i,j i:j \implies (\exists m,n s.t. m:i:j:n & m:(i+j):n)
+ \] 
+-}
+
+prop_IAaxiomM4_1 :: M2set -> Property
+prop_IAaxiomM4_1 x = 
+   ((m `meets` i && i `meets` j && j `meets` n) &&
+    (m `meets` ij && ij `meets` n)) === True
+   where i = m21 x
+         j = m22 x
+         m = validInterval (begin i - 1) (begin i)
+         n = validInterval (end j) (end j + 1)
+         ij = fromJust $ i .+. j
+
+
+{-
+* Interval Relation property testing 
+-}
+
+prop_IAbefore :: Intrvl Int -> Intrvl Int -> Property
 prop_IAbefore i j = 
   IA.before i j ==> (i `meets` k) && (k `meets` j)
-    where k = period (end i) (begin j)
+    where k = validInterval (end i) (begin j)
 
-prop_IAstarts:: Period -> Period -> Property
+
+prop_IAstarts:: Intrvl Int -> Intrvl Int -> Property
 prop_IAstarts i j
   | ((IA.starts i j) == True) =
-    let k = period (begin i) (end j)
+    let k = validInterval (end i) (end j)
     in 
-     ([j] == [i] <<>> [k]) === True
+     (j == (fromJust $ i .+. k)) === True
   | otherwise = IA.starts i j === False
 
-prop_IAfinishes:: Period -> Period -> Property
+
+prop_IAfinishes:: Intrvl Int -> Intrvl Int -> Property
 prop_IAfinishes i j
   | ((IA.finishes i j) == True) =
-    let k = period (begin j) (begin i)
+    let k = validInterval (begin j) (begin i)
     in 
-     ([j] == [k] <<>> [i]) === True
+     (j == (fromJust $ k .+. i)) === True
   | otherwise = IA.finishes i j === False
 
-prop_IAoverlaps:: Period -> Period -> Property
+prop_IAoverlaps:: Intrvl Int -> Intrvl Int -> Property
 prop_IAoverlaps i j
   | ((IA.overlaps i j) == True) = 
-    let k = period (begin i) (begin j)
-        l = period (begin j) (end i)
-        m = period (end i)   (end j)
+    let k = validInterval (begin i) (begin j)
+        l = validInterval (begin j) (end i)
+        m = validInterval (end i)   (end j)
     in 
-     ((i == period (begin k) (end l)) &&
-      (j == period (begin l) (end m))) === True
+     ((i == (fromJust $ k .+. l )) &&
+      (j == (fromJust $ l .+. m ))) === True
   | otherwise  = IA.overlaps i j === False 
 
-prop_IAduring:: Period -> Period -> Property
+prop_IAduring:: Intrvl Int -> Intrvl Int -> Property
 prop_IAduring i j
   | ((IA.during i j) == True) = 
-    let k = period (begin j) (begin i)
-        l = period (end i) (end j)
+    let k = validInterval (begin j) (begin i)
+        l = validInterval (end i) (end j)
     in 
-     ([j] == [k] <<>> [i] <<>> [l]) === True
+     (j == (fromJust $ (fromJust $ k .+. i) .+. l)) === True
   | otherwise  = IA.during i j === False 
-  
+
 
 main :: IO ()
 main = hspec $ do
   describe "Interval Algebra Axioms" $ --modifyMaxDiscardRatio (* 10) $
     do 
-      it "M1" $ property prop_IAaxiomM1
       {- 
       if two periods both meet a third, 
       then any period met by one must also be met by the other.
       -}
-      it "M2" $ property prop_IAaxiomM2
+      it "M1" $ property prop_IAaxiomM1
+
       {- 
         if period i meets period j and period k meets l, 
         then exactly one of the following holds:
           1) i meets l; 
           2) there is an m such that i meets m and m meets l; 
           3) there is an n such that k meets n and n meets j.
-      -} 
-      it "ML1" $ property prop_IAaxiomML1
+      -}       
+      it "M2" $ property prop_IAaxiomM2
+
       {-
         a period cannot meet itself
       -}
-      it "ML2" $ property prop_IAaxiomML2
+      it "ML1" $ property prop_IAaxiomML1
+
       {-
         if i meets j then j does not meet i
       -}
-      -- it "ML3"
+      it "ML2" $ property prop_IAaxiomML2
+
       {-
         For all i, there does not exist m such that i meets m and m meet i
         Not testing that this axiom holds, as I'm not sure how I would
       -}
-      it "M3" $ property prop_IAaxiomM3
+      --it "ML3" $ property prop_IAaxiomML3
+
       {-
         for all periods i there exist periods j and k such that j:i:k
       -}
-      it "M4" $ property prop_IAaxiomM4
+      it "M3" $ property prop_IAaxiomM3
+
+
       {-
         if i meets j then there exists k, m, n such that m:i:j:n and m:k:n
       -}
-      it "M5" $ pending 
+      it "M4" $ property prop_IAaxiomM4
+
+      
       {-
       -}
+      it "M5" $ pending 
+      
+      {-
+        if i meets j then there exists k, m, n such that m:i:j:n and m:k:n
+      -}
+      it "M4.1" $ property prop_IAaxiomM4_1
+ 
 
-  describe "Interval Algebra relations" $ --modifyMaxDiscardRatio (* 10) $
+  describe "Interval Algebra relations" $ 
+      modifyMaxSuccess (*10) $
+      --modifyMaxDiscardRatio (* 10) $
   -- https://en.wikipedia.org/wiki/Allen%27s_interval_algebra#Relations 
   -- contains a visual of these relations
    do
@@ -216,14 +350,14 @@ main = hspec $ do
       it "finishes" $ property prop_IAfinishes
       it "overlaps" $ property prop_IAoverlaps
       it "during"   $ property prop_IAduring
-  
+ {- 
   describe "Period expansions" $ 
     do
       it "expandl safely shrinks a period" $
         expandl (-10) (period 0 10) `shouldBe` period 0 10
       it "expandr safely shrinks a period" $
         expandr (-10) (period 0 10) `shouldBe` period 0 10
-
+-}
 {- Examples of unit testing
   describe "after" $ do
     it "return False for a period before another" $
